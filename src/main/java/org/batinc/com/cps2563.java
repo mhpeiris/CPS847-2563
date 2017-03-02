9ad42fa47539440099581fadd4aa82c6c3f54ef8
Hareen Peiris <hareenmpeiris@gmail.com>
2017-03-01 20:01:10 -0500

    html and java

---
+++ src/main/java/org/batinc/com/cps2563.java
@@ -0,0 +1,6 @@
+public class cps2563 {
+
+	public int multi(int y) {
+		return y *2;
+	}
+}
---
+++ src/test/java/org/batinc/com/Tester.java
@@ -0,0 +1,42 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Tester 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Tester( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Tester.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    public void testEcho1() {
+		cps2563 cps = new cps2563();
+    	assertEquals(2, cps.multi(2));
+	}
+	
+}
